56be972721e1914293e4247968a91bb7137fb9e9
Vincent <vincentwang@skyrec.cc>
2019-12-17 21:32:13 +0800

    feature(PcServer): Change sending format of phone_client to json.

--- PcServer/phone_client.py
+++ PcServer/phone_client.py
@@ -5,6 +5,7 @@ Simple client to emulate the phone
 import socket
 import sys
 import json
+import common
 
 SERVER_IP = '192.168.66.105'
 
@@ -55,32 +56,30 @@ def main():
     '''
     %
     '''
-    get_request = json.loads(GET_REQ)
-    start_request = json.loads(START_REQ)
-    stop_request = json.loads(STOP_REQ)
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     activate = False
     input('Press enter to start...')
-
+    msg = common.get_initial_msg('PHONE')
+    msg['request'] = 'START'
     s.connect((SERVER_IP, PORT))
-    s.send(json.dumps(get_request).encode())
+    s.send(json.dumps(msg).encode())
     print('Sent START to server!')
     rcv_raw = s.recv(512).decode('utf-8')
-    rcv_json = json.loads(rcv_raw)
-    if rcv_raw.split(',')[-1] == 'OK':
-        activate = True
+    activate = True
     print('Started sending data!')
     while activate is True:
         print('-'*50)
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((SERVER_IP, PORT))
         try:
-            s.send(f'{HEADER},GET'.encode())
-            send_time, latitude, longitude, moved = s.recv(
-                512).decode('utf-8').split(',')
-            print(f'Current position: {latitude}, {longitude}')
-            print(f'Moved: {moved}')
+            msg = common.get_initial_msg('PHONE')
+            msg['request'] = 'GET'
+            s.send(json.dumps(msg).encode())
+            rcv_raw = s.recv(512).decode('utf-8')
+            rcv_json = json.loads(rcv_raw)
+
+            print(f'Current position: {rcv_json["position"]}')
         except socket.error():
             print('Socket error!')
         input('Press ENTER to send another phone request...')
